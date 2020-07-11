9a730feb552209af7ea4d039f68980491e26075b
Zedran <snipes564@gmail.com>
2020-07-11 15:09:31 +0200

    better readability

--- mk.bat
+++ mk.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
 
+SET flags=-trimpath -ldflags "-s -w"
 SET output_name="pw.exe"
 SET src_dir=./src
 
 go test %src_dir% -test.v
 
-IF %ERRORLEVEL% EQU 0 go build -o %output_name% -trimpath -ldflags "-s -w" %src_dir%
+IF %ERRORLEVEL% EQU 0 (
+    go build %flags% -o %output_name% %src_dir%
+)
