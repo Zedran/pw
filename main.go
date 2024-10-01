f9a9eca426b83b799af89238fcc6da6a556581dc
Zedran <17592740+Zedran@users.noreply.github.com>
2024-10-01 22:05:53 +0200

    Revisit flag usage strings

--- main.go
+++ main.go
@@ -10,16 +10,16 @@ func main() {
 	log.SetFlags(0)
 
 	var (
-		mode     = flag.String("m", "a", "mode:\n    a - alphanumeric\n    d - diceware\n    n - numeric")
-		pwLen    = flag.Int("l", DEFAULT_LENGTH, fmt.Sprintf("password length, max %d characters", MAX_PW_LENGTH))
-		sep      = flag.String("s", DEFAULT_SEP, "word separator for diceware mode")
-		wordList = flag.String("f", DEFAULT_WL, "file with word list for diceware mode")
+		mode     = flag.String("m", "a", "Generation mode:\n    a - alphanumeric\n    d - diceware\n    n - numeric\n")
+		pwLen    = flag.Int("l", DEFAULT_LENGTH, fmt.Sprintf("Password length, <1; %d> characters", MAX_PW_LENGTH))
+		sep      = flag.String("s", DEFAULT_SEP, "Word separator for diceware mode")
+		wordList = flag.String("f", DEFAULT_WL, "Word list file for diceware mode")
 	)
 
 	flag.Parse()
 
 	if !ValidateLength(*pwLen) {
-		log.Fatalf("length must be an integer in range (0; %d>\n", MAX_PW_LENGTH)
+		log.Fatalf("length must be an integer in range <1; %d>\n", MAX_PW_LENGTH)
 	}
 
 	var p string
