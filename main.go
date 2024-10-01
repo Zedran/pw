9cf87750ac761f930afec4657b43dd60a5ea928a
Zedran <17592740+Zedran@users.noreply.github.com>
2024-10-01 22:41:44 +0200

    Add -n flag that prevents printing LF at the end

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	var (
 		mode     = flag.String("m", "a", "Generation mode:\n    a - alphanumeric\n    d - diceware\n    n - numeric\n")
+		noLF     = flag.Bool("n", false, "Do not print an LF character at the end")
 		pwLen    = flag.Int("l", DEFAULT_LENGTH, fmt.Sprintf("Password length, <1; %d> characters", MAX_PW_LENGTH))
 		sep      = flag.String("s", DEFAULT_SEP, "Word separator for diceware mode")
 		wordList = flag.String("f", DEFAULT_WL, "Word list file for diceware mode")
@@ -33,5 +34,10 @@ func main() {
 	default:
 		log.Fatal("invalid mode argument\n")
 	}
-	fmt.Println(p)
+
+	if *noLF {
+		fmt.Print(p)
+	} else {
+		fmt.Println(p)
+	}
 }
