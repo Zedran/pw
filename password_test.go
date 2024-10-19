b17481c6dac3b7a1797550f9fb5cddb535ef354a
Zedran <17592740+Zedran@users.noreply.github.com>
2024-10-19 15:05:53 +0200

    password_test.go: sort functions alphabetically

--- password_test.go
+++ password_test.go
@@ -7,6 +7,33 @@ import (
 	"github.com/Zedran/pw/internal/tests"
 )
 
+func TestCompileCharset(t *testing.T) {
+	errCases := []string{
+		"",    // Empty set
+		"AA",  // Duplicate specifier
+		"aAd", // Undefined specifier
+	}
+
+	for _, c := range errCases {
+		if out, err := compileCharset(c, ""); err == nil {
+			t.Fatalf("error not returned, output: %s", out)
+		}
+	}
+
+	set, err := compileCharset("n", "1")
+	if err != nil {
+		t.Fatalf("error returned for set 'n', exclusions '1': %v", err)
+	}
+
+	if strings.Count(set, "1") > 0 {
+		t.Fatalf("'1' not excluded from charset: %s", set)
+	}
+
+	if _, err = compileCharset("n", "0123456789"); err == nil {
+		t.Fatalf("complete charset exclusion did not cause an error")
+	}
+}
+
 func TestPasswordBias(t *testing.T) {
 	const (
 		P_LEN       = 20
@@ -40,32 +67,5 @@ func TestPasswordBias(t *testing.T) {
 		t.Fatalf(FMT, chisqr, ">", CRIT_VALUE, "-- biased")
 	} else {
 		t.Logf(FMT, chisqr, "<", CRIT_VALUE, "")
-	}
-}
-
-func TestCompileCharset(t *testing.T) {
-	errCases := []string{
-		"",    // Empty set
-		"AA",  // Duplicate specifier
-		"aAd", // Undefined specifier
-	}
-
-	for _, c := range errCases {
-		if out, err := compileCharset(c, ""); err == nil {
-			t.Fatalf("error not returned, output: %s", out)
-		}
-	}
-
-	set, err := compileCharset("n", "1")
-	if err != nil {
-		t.Fatalf("error returned for set 'n', exclusions '1': %v", err)
-	}
-
-	if strings.Count(set, "1") > 0 {
-		t.Fatalf("'1' not excluded from charset: %s", set)
-	}
-
-	if _, err = compileCharset("n", "0123456789"); err == nil {
-		t.Fatalf("complete charset exclusion did not cause an error")
 	}
 }
